bdc94e841b506fe263f3a72cfc3e72ef7f61ff27
yassirrchi <yassirrchi7@gmail.com>
2023-03-08 16:51:15 +0100

    🔖initial commit

---
+++ src/main/java/com/erpfinance/erpfinance/ErpFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.erpfinance.erpfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erpfinance/erpfinance/ErpFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erpfinance.erpfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
